669cda4092c7fc1e36cfb2d5e7d9c807af04b984
Eric Sorrells <ericsorrells@hotmail.com>
2015-02-16 21:08:37 -0500

    added questions controller & model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216163516) do
+ActiveRecord::Schema.define(version: 20150217010213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.integer  "sequence"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
