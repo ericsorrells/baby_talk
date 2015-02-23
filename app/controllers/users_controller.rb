b5ff40c4868ac730b040317e02fe8513f7b22d2d
Eric Sorrells <ericsorrells@hotmail.com>
2015-02-22 19:03:46 -0500

    auth1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,10 @@ class UsersController < ApplicationController
   before_action :signed_in_user,  only: [:edit, :update, :destroy]
 
   #calls correct_user to determine if user should have access to the page before it loads
-  before_action :correct_user,    only: [:edit, :update, :show] 
+  before_action :correct_user,    only: [:edit, :update] 
 
   #before page is loaded, checks if admin and redirects if not
-  before_action :admin_user, only: :destroy
+  before_action :admin_user, only: [:destroy]
 
   def index
     @users = User.all
@@ -27,27 +27,33 @@ class UsersController < ApplicationController
     if @user.save
       sign_in @user
       flash[:success] = "Account Successfully Created!"
-      redirect_to @user
+      redirect_to user_path(@user)
     else
-      render 'new'
+      render 'edit'
     end
   end
 
   def edit
+    puts "EDIT START"
+      @user = User.find(params[:id])
+    puts "EDIT END: #{@user.name}"
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
+      puts "UPDATED: #{@user.name}"
       flash[:success] = "Profile Updated!"
-      redirect_to @user
+      redirect_to user_path @user
     else
+      puts "NO UPDATE: #{@user.name}"
       render 'edit'
     end
   end
 
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted."
+    flash[:success] = "User Deleted!"
     #redirect_to users_url
   end
 
@@ -62,13 +68,13 @@ class UsersController < ApplicationController
       #gets user, and compares it to current user
       ##if different, redirects to home page
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
+      puts "CORRECT USER [before]: #{@user.id}"
+      redirect_to(root_path) unless current_user?(@user)
+      puts "CORRECT USER [after]: #{@user.id}"
     end
 
     def admin_user
       #unless an admin, it will redirect to root URL
      redirect_to(root_url) unless current_user.admin?
     end
-
-
 end
