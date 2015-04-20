4c25dc3ea167de4102a09045b0986e9002f0def6
Eric Sorrells <ericsorrells@hotmail.com>
2015-04-20 11:39:43 -0400

    changes for showcase 1

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
 
   def self.events(event_category, event_keyword, event_city, event_state, event_time)
-    response = HTTParty.get "https://www.eventbriteapi.com/v3/events/search/?q=#{event_keyword}&venue.city=#{event_city}&venue.region=#{event_state}&categories=#{event_category}&date_modified.keyword=#{event_time}&token=2JHYIBZXSHPPY7PLRDSO"
+    response = HTTParty.get "https://www.eventbriteapi.com/v3/events/search/?q=#{event_keyword}&venue.city=#{event_city}&venue.region=#{event_state}&categories=#{event_category}&date_modified.keyword=#{event_time}&token=CXXISHT3MSLSOOEHGEJN"
     return response['events']
   end
   
