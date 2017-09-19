8b1b2b35a907e7b52563d491255941e3710ae85f
Eric Sorrells <ericsorrells@hotmail.com>
2017-09-19 16:50:12 -0400

    added LetsEncrypt SSL cert info

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,8 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def letsencrypt
+    render text: "rddKfRpWDF4GiE80D488PaAxIpP_n1DKmmQGTnuYsds.NgQk8Q5od9gEZhsQP3Whs-FgUjAlulxVoxEGytLb1q4"
+  end
 end
